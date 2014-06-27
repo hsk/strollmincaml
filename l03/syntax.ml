e79570ef6716f6e31352ea8a42ff65368fc9265f
Hiroshi Sakurai <sakurai777@gmail.com>
2014-06-27 14:10:32 +0900

    changed readme files.

--- l03/syntax.ml
+++
@@ -1,6 +0,0 @@
-type t =
-  | Int of int
-  | Add of t * t
-  | Sub of t * t
-  | Let of t * t
-  | Print of t
--- l04/syntax.ml
+++
@@ -1,8 +0,0 @@
-type t =
-  | Int of int
-  | Add of t * t
-  | Sub of t * t
-  | Let of (string * Type.t) * t * t
-  | Unit
-  | Var of string
-  | Print of t
